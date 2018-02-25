e127ac24253beec4f4e374c6bd23e54a137715ea
Patrick Rawlings <ptyrawlings@gmail.com>
2018-02-24 23:47:33 -0500

    Successfully got the API saving csvs

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from . import views
-from api.views import list
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    # TODO: Update this name to api/file/create
-    url(r'^list/$', list, name='list'),
+    url(r'^api/files/upload$', views.ImportFileUpload.as_view()),
     url(r'^admin/', admin.site.urls),
     url(r'^api/rows', views.ImportRowIndex.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -19,54 +19,37 @@ class ImportRowIndex(APIView):
         serializer = ImportRowSerialzier(import_rows, many=True)
         return Response(serializer.data)
 
-def list(request):
-    # Load documents for the list page
-    documents = ImportFile.objects.all()
+class ImportFileUpload(APIView):
+    def post(self, request):
+        uploaded_file = request.FILES['docfile']
+        newdoc = ImportFile(docfile=uploaded_file,filename=uploaded_file.name)
+        newdoc.save() # validate here
+        newdoc.docfile.compress() # After saving the file compress it
 
-    # Handle file upload
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            uploaded_file = request.FILES['docfile']
-            newdoc = ImportFile(docfile=uploaded_file,filename=uploaded_file.name)
-            newdoc.save # validate here
-            newdoc.docfile.compress() # After saving the file compress it
+        # Read the contents of the uploaded file
+        file_reader = csv.DictReader(uploaded_file, delimiter=',')
+        next(file_reader, None) # skip the headers row
+        for row in file_reader:
+            self.parse_row(row, newdoc) # Create an Import Row Object for each CSV row
+        return Response({'status': 200 })
 
-            # Read the contents of the uploaded file
-            file_reader = csv.DictReader(uploaded_file, delimiter=',')
-            next(file_reader, None) # skip the headers row
-            for row in file_reader:
-                parse_row(row, newdoc) # Create an Import Row Object for CSV row
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('list'))
-    else:
-        form = DocumentForm()  # A empty, unbound form
-
-    # Render list page with the documents and the form
-    return render(
-        request,
-        'list.html',
-        {'documents': documents, 'form': form}
-    )
-
-def parse_row(row, uploaded_file):
-    ImportRow.objects.create(
-        file=uploaded_file,
-        date=datetime.strptime(row["Observation Date"], '%m/%d/%Y'),
-        retail_sales_volume=row['Retail Sales Volume'],
-        retail_price=row['Retail Price'],
-        acv_dist=row['ACV Distribution'],
-        competition=row['Competition'],
-        edlp=row['EDLP'],
-        trade_any_promo=row['Trade Any Promo'],
-        tv=row['TV'],
-        print_value=row['Print'],
-        online_display=row['Online Display'],
-        preroll_video=row['Pre-roll Video'],
-        paid_search=row['Paid Search'],
-        public_relations=row['Public Relations'],
-        fsi=row['FSI'],
-        digital_coupon=row['Digital Coupon'],
-        shopper_marketing=row['Shopper Marketing'],
-    ) 
+    def parse_row(self, row, uploaded_file):
+        ImportRow.objects.create(
+            file=uploaded_file,
+            date=datetime.strptime(row["Observation Date"], '%m/%d/%Y'),
+            retail_sales_volume=row['Retail Sales Volume'],
+            retail_price=row['Retail Price'],
+            acv_dist=row['ACV Distribution'],
+            competition=row['Competition'],
+            edlp=row['EDLP'],
+            trade_any_promo=row['Trade Any Promo'],
+            tv=row['TV'],
+            print_value=row['Print'],
+            online_display=row['Online Display'],
+            preroll_video=row['Pre-roll Video'],
+            paid_search=row['Paid Search'],
+            public_relations=row['Public Relations'],
+            fsi=row['FSI'],
+            digital_coupon=row['Digital Coupon'],
+            shopper_marketing=row['Shopper Marketing'],
+        )

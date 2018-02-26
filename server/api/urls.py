34f4159e2e54331b88466e6bcf473924c5bab3ea
Patrick Rawlings <ptyrawlings@gmail.com>
2018-02-25 23:23:39 -0500

    Change the Admin url back to normal

--- server/api/urls.py
+++ server/api/urls.py
@@ -7,6 +7,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'^api/files/upload$', views.ImportFileUpload.as_view()),
-    url(r'^', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^api/rows', views.ImportRowIndex.as_view()),
 ]
